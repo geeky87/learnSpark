dc9f8e88aa95222856585e8828b3a4e05647ce50
acharya <acharya87@hotmail.com>
2019-08-12 07:38:06 +0200

    My Setup Commit

--- src/main/scala/MyDataFrame.scala
+++ src/main/scala/MyDataFrame.scala
@@ -1,3 +1,11 @@
 object MyDataFrame extends App with Context {
+val dfTags = sparkSession
+  .read
+  .option("Header","true")
+  .option("inferschema","true")
+  .csv("src/resources/question_tags_10K.csv")
+  .toDF("id","tag")
+
+  dfTags.show(10)
 
 }
